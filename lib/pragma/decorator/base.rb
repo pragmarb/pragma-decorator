acba46ab6e10fefb6c6e1f4036c8cb6ee2301849
Alessandro Desantis <desa.alessandro@gmail.com>
2016-11-27 14:21:32 +0100

    Fix coding style issues

--- lib/pragma/decorator/base.rb
+++ lib/pragma/decorator/base.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
 require 'roar/decorator'
 require 'roar/json'
 
 module Pragma
   module Decorator
+    # This is the base decorator that all your resource-specific decorators should extend from.
+    #
+    # It is already configured to render your resources in JSON.
+    #
+    # @author Alessandro Desantis
     class Base < Roar::Decorator
       feature Roar::JSON
     end
