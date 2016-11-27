f6475f43b8153ad7d5c5bc4f08186f11b69ed0cd
Alessandro Desantis <desa.alessandro@gmail.com>
2016-11-27 16:24:28 +0100

    Remove old require

--- lib/pragma/decorator.rb
+++ lib/pragma/decorator.rb
@@ -3,7 +3,6 @@ require 'roar'
 
 require 'pragma/decorator/version'
 require 'pragma/decorator/base'
-require 'pragma/decorator/collection'
 
 module Pragma
   # Represent your API resources in JSON with minimum hassle.
