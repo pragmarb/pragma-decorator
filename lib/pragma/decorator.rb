27b0c9b583bd2fa3741aa46e9b4cc7d7e2e1fe02
Alessandro Desantis <desa.alessandro@gmail.com>
2016-11-27 16:16:47 +0100

    Add author to Pragma::Decorator module

--- lib/pragma/decorator.rb
+++ lib/pragma/decorator.rb
@@ -3,9 +3,12 @@ require 'roar'
 
 require 'pragma/decorator/version'
 require 'pragma/decorator/base'
+require 'pragma/decorator/collection'
 
 module Pragma
   # Represent your API resources in JSON with minimum hassle.
+  #
+  # @author Alessandro Desantis
   module Decorator
   end
 end
