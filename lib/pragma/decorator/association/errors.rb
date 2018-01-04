71ac5890f8fe909823f5bd982520b77dfbd7229d
Alessandro Desantis <desa.alessandro@gmail.com>
2018-01-04 12:33:19 +0100

    Improve performance of Association

--- lib/pragma/decorator/association/errors.rb
+++ lib/pragma/decorator/association/errors.rb
@@ -25,6 +25,26 @@ module Pragma
           super "The '#{child}' association is expanded, but its parent '#{parent}' is not."
         end
       end
+
+      # This error is raised when an association's type is different from its type as reported by
+      # the model's reflection.
+      #
+      # @author Alessandro Desantis
+      class InconsistentTypeError < StandardError
+        # Initializes the error.
+        #
+        # @param decorator [Base] the decorator where the association is defined
+        # @param reflection [Reflection] the reflection of the inconsistent association
+        # @param model_type [Symbol|String] the real type of the association
+        def initialize(decorator:, reflection:, model_type:)
+          message = <<~MSG.tr("\n", ' ')
+            #{decorator.class}: Association #{reflection.property} is defined as #{model_type} on
+            the model, but as #{reflection.type} in the decorator.
+          MSG
+
+          super message
+        end
+      end
     end
   end
 end
