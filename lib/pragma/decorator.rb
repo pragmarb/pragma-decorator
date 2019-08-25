acb03b89c1ae00c5faccd608b4da6d56cb0c5aa9
Alessandro Desantis <desa.alessandro@gmail.com>
2019-08-25 18:35:07 +0200

    Switch to Zeitwerk for autoloading

--- lib/pragma/decorator.rb
+++ lib/pragma/decorator.rb
@@ -1,33 +1,15 @@
 # frozen_string_literal: true
 
-require 'roar'
 require 'adaptor'
+require 'roar'
+require 'zeitwerk'
 
-require 'pragma/decorator/version'
-
-require 'pragma/decorator/base'
-
-require 'pragma/decorator/association'
-require 'pragma/decorator/association/reflection'
-require 'pragma/decorator/association/bond'
-require 'pragma/decorator/association/adapter/base'
-require 'pragma/decorator/association/adapter/active_record'
-require 'pragma/decorator/association/adapter/poro'
-require 'pragma/decorator/association/adapter'
-require 'pragma/decorator/association/errors'
-
-require 'pragma/decorator/timestamp'
-
-require 'pragma/decorator/type'
-
-require 'pragma/decorator/collection'
-
-require 'pragma/decorator/pagination'
-
-require 'pragma/decorator/pagination/adapter/base'
-require 'pragma/decorator/pagination/adapter/kaminari'
-require 'pragma/decorator/pagination/adapter/will_paginate'
-require 'pragma/decorator/pagination/adapter'
+Zeitwerk::Loader.new.tap do |loader|
+  loader.tag = File.basename(__FILE__, '.rb')
+  loader.inflector = Zeitwerk::GemInflector.new(__FILE__)
+  loader.push_dir(File.expand_path('..', __dir__))
+  loader.setup
+end
 
 module Pragma
   # Represent your API resources in JSON with minimum hassle.
--- pragma-decorator.gemspec
+++ pragma-decorator.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'roar', '~> 1.0'
   spec.add_dependency 'multi_json', '~> 1.12'
   spec.add_dependency 'adaptor', '~> 0.2.1'
+  spec.add_dependency 'zeitwerk', '~> 2.1'
 
   spec.add_development_dependency 'bundler'
   spec.add_development_dependency 'rake'
